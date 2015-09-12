ee357827700c50a4c9c5f53a978f7af0f8ff43cd
Oswald Berthold <opt@sdfk.de>
2015-09-12 23:44:57 +0200

    - getting along with settings and setting indices

--- sw/ground_segment/python/paramopt.py
+++ sw/ground_segment/python/paramopt.py
@@ -44,9 +44,12 @@ class MyAgent(object):
         # self.timer = threading.Timer(0.1, self.update_leds)
         # self.timer.start()
 
+        # activity control
+        self.active = False
         # data collection etc
         self.cnt_eval = 0 # count number of evaluations of cost
         self.cnt_time = 0
+        self.cnt_setting = 0
         self.maxsamp = 100
         self.numdata = 3 * 2 # sp, ref, measurement * 2 for attitude + batt, mode
         self.logdata = np.zeros((self.maxsamp, self.numdata))
@@ -91,6 +94,9 @@ class MyAgent(object):
         # if time.time() - self.aircrafts[ac_id].messages[msg.name].last_seen < 0.2:
         # return
         # print("ac_id", ac_id)
+        if not self.active:
+            return
+
         if msg.msg_class == "telemetry":
             # print("msg", dir(msg))
             print("msg", msg.name)
@@ -99,25 +105,47 @@ class MyAgent(object):
             print("msg", msg.fieldcoefs)
             # pass
 
-        print(type(msg.fieldvalues[0]))
+        # print(type(msg.fieldvalues[0]))
 
+        # stash and save logdata
         if msg.name == "STAB_ATTITUDE_INT":
             self.logdata[self.cnt_time,2] = float(msg.fieldvalues[3]) * msg.fieldcoefs[3] # est_phi
             self.logdata[self.cnt_time,5] = float(msg.fieldvalues[4]) * msg.fieldcoefs[4] # est_theta
+            # increment counter
             self.cnt_time += 1
         elif msg.name == "STAB_ATTITUDE_REF_INT":
             self.logdata[self.cnt_time,0] = float(msg.fieldvalues[0]) * msg.fieldcoefs[0] # sp_phi
             self.logdata[self.cnt_time,1] = float(msg.fieldvalues[3]) * msg.fieldcoefs[3] # ref_phi
             self.logdata[self.cnt_time,3] = float(msg.fieldvalues[1]) * msg.fieldcoefs[1] # sp_theta
             self.logdata[self.cnt_time,4] = float(msg.fieldvalues[4]) * msg.fieldcoefs[4] # ref_theta
+
+        # finish episode
         if self.cnt_time == self.maxsamp:
+            # compute something
+            self.active = False
+            self.cnt_time = 0
+            pl.cla()
             pl.plot(self.logdata)
+            
             pl.show()
-            self.shutdown_handler(15, "")
+            pl.draw()
+            
+            # self.shutdown_handler(15, "")
             
     def objective(self, params):
-        print(objective)
-            
+        # suggest new setting
+        self.settings_if.lookup[38].value = 850 + np.random.randint(10)
+        # send setting to autopilot
+        self.settings_if.SendSetting(38)
+        # tell agent about new setting
+        self.cnt_setting += 1
+        # evaluate
+        self.active = True
+        # print(objective)
+        while self.active:
+            time.sleep(1.)
+        # print evaluation result
+        print("cost %f" % np.mean(np.square(self.logdata[:,1] - self.logdata[:,2])))
     
 def main(args):
     print("main:", args)
@@ -129,6 +157,8 @@ def main(args):
     # print("sent to ac %d idx: %d, value: %d" % (args.ac_id[0], index, ivyinterface.lookup[index].value))
 
     # a = 
+
+    pl.ion()
     
     a = MyAgent("agent", int(args.ac_id))
 
@@ -137,7 +167,10 @@ def main(args):
     # print("dir(a)", dir(a))
     a.start_settings()
     a.start()
-        
+
+    # wait for ivy to come up
+    time.sleep(3.)
+            
     # sys.exit(0)
     # time.sleep(3)
     # print("sending ...")
@@ -145,15 +178,19 @@ def main(args):
     # print ("sent")
     # a.send_msg("dl DL_SETTING %d %d %d" % (args.ac_id[0], index, 317))
     # print ("sent")
+
+    # set telemetry mode to attitude loop
+    a.settings_if.lookup[0].value = 7
+    # send setting to autopilot
+    a.settings_if.SendSetting(0)
     
     while True:
-        print("blub")
+        print("Starting episode")
         # print("Setting Att Loop pgain phi(%d) = %f" % (38, a.settings_if.lookup[38].value))
         # for i 
-        print(a.settings_if.lookup[38].value)
-        a.settings_if.lookup[38].value = 850 + np.random.randint(10)
-        a.settings_if.SendSetting(38)
-        time.sleep(1.)
+        print("settings (read):", a.settings_if.lookup[38].value)
+        a.objective({})
+        time.sleep(5.)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
