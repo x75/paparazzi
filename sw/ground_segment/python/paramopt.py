438772096cba1fd7100b0a876be3890d60257667
Oswald Berthold <opt@sdfk.de>
2015-09-15 20:04:29 +0200

    - testing paramopt: basically works, occasionally dl_setting messages are dropped and the new settings don't end up on the AP, need to fix that

--- sw/ground_segment/python/paramopt.py
+++ sw/ground_segment/python/paramopt.py
@@ -20,6 +20,8 @@ import pylab as pl
 import tables as tb
 
 from paramopt_data import PPRZ_Opt_Attitude
+
+import pygame
     
 # class MyAgent(IvyServer):
 class MyAgent(object):
@@ -49,14 +51,17 @@ class MyAgent(object):
 
         # activity control
         self.active = False
+        self.safety = False
         # data collection etc
         self.cnt_eval = 0 # count number of evaluations of cost
         self.cnt_time = 0
         self.cnt_setting = 0
         self.maxsamp = 100
-        self.numdata = 3 * 2 # sp, ref, measurement * 2 for attitude + batt, mode
+        self.numdata = 3 * 2 + 6 # sp, ref, measurement * 2 for attitude + 6 RC channels
         self.logdata = np.zeros((self.maxsamp, self.numdata))
 
+        # parameters optimized
+        self.param_idx = [29, 30, 31, 32, 33, 34, 35, 36]
         # backup parameters
         self.default_params = (
             300, # pgain_phi
@@ -72,7 +77,7 @@ class MyAgent(object):
 
         # data storage
         self.storage_version = "v2"
-        self.tblfilename = "paramopt_ppz_v1.h5"
+        self.tblfilename = "paramopt_ppz_v2.h5"
         self.h5file = tb.open_file(self.tblfilename, mode = "a")
         # check if top group exists
         try:
@@ -127,9 +132,7 @@ class MyAgent(object):
         # if time.time() - self.aircrafts[ac_id].messages[msg.name].last_seen < 0.2:
         # return
         # print("ac_id", ac_id)
-        if not self.active:
-            return
-
+        
         if msg.msg_class == "telemetry":
             # print("msg", dir(msg))
             # print("msg", msg.name)
@@ -142,26 +145,40 @@ class MyAgent(object):
         # print(type(msg.fieldvalues[0]))
 
         # stash and save logdata
+        if msg.name == "RC":
+            # print("RC")
+            rc_vals = msg.fieldvalues[0].split(",")
+            if self.cnt_time < self.maxsamp:
+                self.logdata[self.cnt_time,6:12] = rc_vals[0:6]
+            # print("RC", len(rc_vals), rc_vals)
+            if int(rc_vals[4]) > 1000:
+                self.active = False
+                if not self.safety:
+                    self.safety = True
+                    self.reset_params()
+                    time.sleep(0.5)
+                    self.reset_params()
+                # self.cnt_time = 0
+            elif int(rc_vals[4]) < -1000:
+                self.safety = False
+            return # can only be one message
+
+        if not self.active:
+            return
+                
         if msg.name == "STAB_ATTITUDE_INT":
             self.logdata[self.cnt_time,2] = float(msg.fieldvalues[3]) * msg.fieldcoefs[3] # est_phi
             self.logdata[self.cnt_time,5] = float(msg.fieldvalues[4]) * msg.fieldcoefs[4] # est_theta
             # increment counter
             self.cnt_time += 1
+            
         elif msg.name == "STAB_ATTITUDE_REF_INT":
             self.logdata[self.cnt_time,0] = float(msg.fieldvalues[0]) * msg.fieldcoefs[0] # sp_phi
             self.logdata[self.cnt_time,1] = float(msg.fieldvalues[3]) * msg.fieldcoefs[3] # ref_phi
             self.logdata[self.cnt_time,3] = float(msg.fieldvalues[1]) * msg.fieldcoefs[1] # sp_theta
             self.logdata[self.cnt_time,4] = float(msg.fieldvalues[4]) * msg.fieldcoefs[4] # ref_theta
-        elif msg.name == "ROTORCRAFT_STATUS":
-            print("STATUS")
-        elif msg.name == "RC":
-            # print("RC")
-            rc_vals = msg.fieldvalues[0].split(",")
-            print("RC", rc_vals)
-            if int(rc_vals[4]) > 1000:
-                self.reset_params()
-                self.active = False
-                # self.cnt_time = 0
+        # elif msg.name == "ROTORCRAFT_STATUS":
+        #     print("STATUS")
 
         # finish episode
         if self.cnt_time == self.maxsamp:
@@ -172,9 +189,18 @@ class MyAgent(object):
 
     def reset_params(self):
         print("reset")
+        for idx in self.param_idx:
+            if self.settings_if.lookup[idx].value == self.default_params[idx - self.param_idx[0]]:
+                print("SAME")
+            self.settings_if.lookup[idx].value = self.default_params[idx - self.param_idx[0]] # 850 + np.random.randint(10)
+            self.settings_if.SendSetting(idx)
+            print("params[%d] = %d" % (idx, self.settings_if.lookup[idx].value))
             
     def objective(self, params):
+        print("params:", params)
+        pygame.mixer.music.play()
         # 29, 30, 31, 32, 33, 34, 35, 36
+        assert(len(params) == len(self.default_params))
         # reset counter
         self.cnt_time = 0
         # reet logdata
@@ -182,9 +208,14 @@ class MyAgent(object):
         # play sound
         
         # suggest new setting
-        self.settings_if.lookup[38].value = params[0] # 850 + np.random.randint(10)
-        # send setting to autopilot
-        self.settings_if.SendSetting(38)
+        for idx in self.param_idx:
+            r_idx = idx - self.param_idx[0]
+            print("r_idx = %d" % r_idx)
+            self.settings_if.lookup[idx].value = params[r_idx]
+            print("params[%d] = %d" % (idx, self.settings_if.lookup[idx].value))
+            # 850 + np.random.randint(10)
+            # send setting to autopilot
+            self.settings_if.SendSetting(idx)
         # tell agent about new setting
         self.cnt_setting += 1
         # evaluate
@@ -193,6 +224,14 @@ class MyAgent(object):
         while self.active:
             time.sleep(1.)
 
+        while self.safety:
+            time.sleep(1)
+            print("Waiting for safety")
+            for idx in self.param_idx:
+                r_idx = idx - self.param_idx[0]
+                # print("r_idx = %d" % r_idx)
+                print(self.settings_if.lookup[idx].value)
+            
         # compute cost
         # premature termination gives max cost
         if self.cnt_time != self.maxsamp:
@@ -223,12 +262,67 @@ class MyAgent(object):
         self.table.flush()
 
         # print evaluation result
+        pl.subplot(211)
         pl.cla()
-        pl.plot(self.logdata)
+        pl.plot(self.logdata[:,0:6])
             
+        pl.subplot(212)
+        pl.cla()
+        pl.plot(self.logdata[:,6:12])
+        
         pl.show()
         pl.draw()
+        print("cost = %f" % cost)
+        time.sleep(1.)
         return cost
+
+def main_loop_manual(args, a):
+    # manual testing main loop
+    while True:
+        print("Starting episode")
+        # print("Setting Att Loop pgain phi(%d) = %f" % (38, a.settings_if.lookup[38].value))
+        # read current settings
+        # for i in range(29, 38):
+        #     print("settings.lookup[%d] (read) = %f" % (i, a.settings_if.lookup[i].value))
+        params = (
+            290 + np.random.randint(20),
+            340 + np.random.randint(20),
+            90 + np.random.randint(20),
+            140 + np.random.randint(20),
+            290 + np.random.randint(20),
+            340 + np.random.randint(20),
+            90 + np.random.randint(20),
+            140 + np.random.randint(20),
+        )
+        cost = a.objective(params)
+        print("cost %f" % cost)
+        # a.interface.stop()
+        time.sleep(5.)
+        # a.interface.start()
+
+def main_loop_hyperopt(args, a):
+    # hyperopt main loop
+    from hyperopt import hp, fmin, tpe, Trials, rand
+    space = [
+        hp.quniform("pgain_phi", 5, 60, 1),
+        hp.quniform("dgain_p", 0, 100, 1),
+        hp.quniform("igain_phi", 5, 120, 1),
+        hp.quniform("ddgain_p", 20, 70, 1),
+        hp.quniform("pgain_theta", 5, 60, 1),
+        hp.quniform("dgain_q", 0, 100, 1),
+        hp.quniform("igain_theta", 5, 120, 1),
+        hp.quniform("ddgain_q", 20, 70, 1),
+        ]
+        
+    trials = Trials()
+    if args.mode == "hpo_rand":
+        best = fmin(a.objective, space, algo=rand.suggest, max_evals=int(args.maxeval), trials=trials)
+    elif args.mode == "hpo_tpe":
+        best = fmin(a.objective, space, algo=tpe.suggest, max_evals=int(args.maxeval), trials=trials)
+    elif args.mode == "hpo_gp":
+        print("implement gp_smbo here")
+        # best = fmin(a.objective, space, algo=tpe.suggest, max_evals=int(args.maxeval), trials=trials)
+    print("best", best)
     
 def main(args):
     print("main:", args)
@@ -242,6 +336,11 @@ def main(args):
     # a = 
 
     pl.ion()
+
+
+    pygame.init()
+
+    pygame.mixer.music.load("paramopt_1.wav")
     
     a = MyAgent("agent", int(args.ac_id))
 
@@ -268,25 +367,16 @@ def main(args):
     # send setting to autopilot
     a.settings_if.SendSetting(0)
 
-    
-    
-    while True:
-        print("Starting episode")
-        # print("Setting Att Loop pgain phi(%d) = %f" % (38, a.settings_if.lookup[38].value))
-        # read current settings
-        # for i in range(29, 38):
-        #     print("settings.lookup[%d] (read) = %f" % (i, a.settings_if.lookup[i].value))
-        params = (290 + np.random.randint(20), 0, 0, 0, 0, 0, 0, 0)
-        cost = a.objective(params)
-        print("cost %f" % cost)
-        # a.interface.stop()
-        time.sleep(5.)
-        # a.interface.start()
-
+    if args.mode == "manual":
+        main_loop_manual(args, a)
+    elif args.mode.startswith("hpo"):
+        main_loop_hyperopt(args, a)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--ac_id", dest="ac_id", default=167)
+    parser.add_argument("-m", "--mode", dest="mode", default="manual")
+    parser.add_argument("-me", "--maxeval", default=60)
     
     args = parser.parse_args()
     main(args)
